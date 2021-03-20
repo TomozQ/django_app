6bcd618f6de4287123b2eca0e28a613ca25029aa
TomozQ <inf.4clover@gmail.com>
2021-03-20 21:03:19 +0900

    NullBoolean

--- hello/views.py
+++ hello/views.py
@@ -8,15 +8,16 @@ class HelloView(TemplateView):
     def __init__(self):
         self.params = {
             'title': 'Hello',
-            'message': 'your data: ',
             'form': HelloForm(),
+            'result': None,
         }
 
     def get(self, request):
         return render(request, 'hello/index.html', self.params)
 
     def post(self, request):
-        msg = 'あなたは、<br>' + request.POST['name'] + '(' + request.POST['age'] + ')<br>さんです。<br>メールアドレスは<br>' + request.POST['mail'] + '<br>ですね。'
-        self.params['message'] = msg
+        chk = request.POST['check']
+        self.params['result'] = 'you selected: ' + chk 
         self.params['form'] = HelloForm(request.POST)
+        
         return render(request, 'hello/index.html', self.params)
