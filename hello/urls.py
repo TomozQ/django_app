a8e3cd923fb03882577fa888c1cf6aba3061f51f
TomozQ <inf.4clover@gmail.com>
2021-03-20 13:38:30 +0900

    initialcommit

---
+++ hello/urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 12:16:22 2021
+
+@author: furuyatomoki
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('', views.index, name='index'),
+        path('next', views.next, name='next'),
+    ]
---
+++ hello/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    params = {
+        'title': 'Hello/Index',
+        'msg': 'これはサンプルで作ったページです。',
+        'goto': 'next',
+    }
+    return render(request, 'hello/index.html', params)
+
+def next(request):
+    params = {
+        'title': 'Hello/Next',
+        'msg': 'これはもう一つのページです。',
+        'goto': 'index',
+    }
+    return render(request, 'hello/index.html', params)
+# Create your views here.
