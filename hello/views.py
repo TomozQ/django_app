cd72352604338b4b0843aa66e3eeb09bf2e23482
TomozQ <inf.4clover@gmail.com>
2021-03-21 13:44:35 +0900

    create friends

--- hello/views.py
+++ hello/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from .models import Friend
-from django.db.models import QuerySet
-
-def __new_str__(self):
-    result = ''
-    for item in self: #item = record
-        result += '<tr>'
-        for k in item: #k = item.key
-            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>' #k->key, item[k]->item.key.value
-        result += '</tr>'
-    return result
-
-QuerySet.__str__ = __new_str__
+from .forms import HelloForm
 
 def index(request):
-    data = Friend.objects.all().values('id', 'name', 'age')
+    data = Friend.objects.all()
     params = {
         'title': 'Hello',
         'data': data,
     }
 
     return render(request, 'hello/index.html', params)
+
+def create(request):
+    params = {
+        'title': 'Hello',
+        'form': HelloForm,
+    }
+    if(request.method == 'POST'):
+        name = request.POST['name']
+        mail = request.POST['mail']
+        gender = 'gender' in request.POST
+        age = request.POST['age']
+        birth = request.POST['birthday']
+
+        friend = Friend(name = name, mail = mail, gender = gender, age = age, birthday = birth)
+        friend.save()
+        return redirect(to = '/hello')
+    
+    return render(request, 'hello/create.html', params)
