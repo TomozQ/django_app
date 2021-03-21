afe713898d6f386ded373419297672a572a5a726
TomozQ <inf.4clover@gmail.com>
2021-03-21 13:10:40 +0900

    querySet

--- hello/views.py
+++ hello/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Friend
-from .forms import HelloForm
 
 def index(request):
-    data = Friend.objects.all()
+    num = Friend.objects.all().count()
+    first = Friend.objects.all().first()
+    last = Friend.objects.all().last()
+    data = [num, first, last]
     params = {
         'title': 'Hello',
-        'message': 'all friends.',
-        'form': HelloForm(),
-        'data': [],
+        'data': data,
     }
-    if(request.method == 'POST'):
-        num = request.POST['id']
-        item = Friend.objects.get(id=num)
-        params['data'] = [item]
-        params['form'] = HelloForm(request.POST)
-    else:
-        params['data'] = Friend.objects.all()
 
     return render(request, 'hello/index.html', params)
