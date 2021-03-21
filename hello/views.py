85de7642593cc0e1ba37598a762542b12fa117db
TomozQ <inf.4clover@gmail.com>
2021-03-21 13:20:52 +0900

    queryset cast str

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Friend
+from django.db.models import QuerySet
+
+def __new_str__(self):
+    result = ''
+    for item in self: #item = record
+        result += '<tr>'
+        for k in item: #k = item.key
+            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>' #k->key, item[k]->item.key.value
+        result += '</tr>'
+    return result
+
+QuerySet.__str__ = __new_str__
 
 def index(request):
-    num = Friend.objects.all().count()
-    first = Friend.objects.all().first()
-    last = Friend.objects.all().last()
-    data = [num, first, last]
+    data = Friend.objects.all().values('id', 'name', 'age')
     params = {
         'title': 'Hello',
         'data': data,
