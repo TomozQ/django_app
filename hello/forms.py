7145d0925d4a1f8c9212f9fb767b44f8489d8200
TomozQ <inf.4clover@gmail.com>
2021-03-21 13:00:41 +0900

    find friends

--- hello/forms.py
+++ hello/forms.py
@@ -1,12 +1,4 @@
 from django import forms
 
 class HelloForm(forms.Form):
-  data = [
-    ('one', 'radio 1'),
-    ('two', 'radio 2'),
-    ('three', 'radio 3'),
-    ('four', 'radio 4'),
-    ('five', 'radio 5'),
-  ]
-
-  choice = forms.MultipleChoiceField(label='radio', choices=data, widget=forms.SelectMultiple(attrs={'size': 6}))
+  id = forms.IntegerField(label='ID')
