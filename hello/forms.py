a8116b908387d7a5008677c7eb5fa2646c5df947
TomozQ <inf.4clover@gmail.com>
2021-03-22 21:52:15 +0900

    some validate

--- hello/forms.py
+++ hello/forms.py
@@ -9,5 +9,13 @@ class FriendForm( forms.ModelForm ):
 class FindForm( forms.Form ):
   find = forms.CharField( label = 'Find', required = False )
 
+# class CheckForm( forms.Form ):
+#   required = forms.IntegerField(label = 'Required')
+#   empty = forms.CharField(label='Empty', empty_value = True)
+#   min = forms.IntegerField(label = 'Min', min_value = 10)
+#   max = forms.IntegerField(label = 'Max', max_value = 10)
+  
 class CheckForm( forms.Form ):
-  str = forms.CharField( label = 'Name' )
+  date = forms.DateField(label = 'Date', input_formats = ['%d'])
+  time = forms.TimeField(label = 'Time')
+  datetime = forms.DateTimeField(label = 'DateTime')
