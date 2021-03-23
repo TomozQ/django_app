7de7238fd785f441824fa3a0091e3e35d21c3371
TomozQ <inf.4clover@gmail.com>
2021-03-23 20:36:29 +0900

    validateFunction

--- hello/models.py
+++ hello/models.py
@@ -1,9 +1,17 @@
+import re
 from django.db import models
-from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator, ValidationError
+
+
+def number_only( value ):
+  if( re.match(r'^[0-9]*$', value) == None):
+    raise ValidationError(
+      '%(value)s is not Number', params = { 'value': value },
+    )
 
 class Friend(models.Model):
   name = models.CharField(
-    max_length = 100, validators = [MinLengthValidator(10)]
+    max_length = 100, validators = [number_only]
   )
   mail = models.EmailField(
     max_length = 200, validators = [MinLengthValidator(10)]
